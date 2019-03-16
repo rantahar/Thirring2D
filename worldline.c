4a886a0292ac57aabebb9024f25ac6b1d03215ec
Jarno Rantaharju <j.m.o.rantaharju@swansea.ac.uk>
2019-03-16 10:24:16 +0000

    Print sector when not running LLR

--- worldline.c
+++ worldline.c
@@ -1211,8 +1211,10 @@ int main(int argc, char* argv[])
         if( m == 0 )
           printf("SUSCEPTIBILITY %g \n", (double)sum_susc_wb/n_average);
         printf("SIGN %g\n", (double)sum_sign/n_average);
-        //for(int s=0; s<MAX_SECTOR; s++)
-        //  printf("SECTOR %d %g \n", s, (double)sectors[s]/n_average);
+        #ifndef LLR
+        for(int s=0; s<MAX_SECTOR; s++)
+          printf("SECTOR %d %g \n", s, (double)sectors[s]/n_average);
+        #endif
 
         double llr_dS = (double)(sectors[llr_target]-sectors[llr_target+1])/n_average;
         LLR_update( llr_dS );
