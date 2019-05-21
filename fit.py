ed5731e3b5a560de44eaa5ecba8a0c7a7601ddbe
j.m.o.rantaharju@swansea.ac.uk <j.m.o.rantaharju@swansea.ac.uk>
2019-05-21 16:12:27 +0100

    Calculate sign. Reduce weight further from the center of the window.

--- fit.py
+++ fit.py
@@ -7,8 +7,8 @@ from scipy.optimize import curve_fit
 import time
 
 
-def fit_function( x, p1, p2, p3, p4 ):
-  return p1 + x*p2 + x*x*p3 + x*x*x*p4
+def fit_function( x, p1, p2, p3 ):
+  return p1 + x*p2 + x*x*p3
 
 
 def plot_window(wl_f, center, width):
@@ -29,30 +29,37 @@ def fit_window( wl_f, center, width ):
 
   window = np.logical_and(x > center-width-1, x < center+width+1)
   window = np.logical_and(window, x >= 0)
-  sigma = np.array(sigma[ window ])
-  
-  wl_f = np.array(wl_f[ :, window ])
-  x = x[window]
+
+  x = x[window]-center
+  wl_f = wl_f[ :, window ]
+  sigma = sigma[ window ] * ( 1+np.abs(x)*2/width )
 
   par = []
   for m in range(wl_f.shape[0]):
-    parameters, conv = curve_fit(fit_function, x, wl_f[m], [0,0,0,0], sigma=sigma)
+    parameters, conv = curve_fit(fit_function, x, wl_f[m], [0,0,0], sigma=sigma)
     par.append(parameters)
-    diff  = (fit_function(x,*par[0]) - wl_f[m])/sigma
+    diff  = (fit_function(x,*par[m]) - wl_f[m])/sigma
     chisq = (diff*diff).sum()
   return par
 
 
 def evaluate_fit(x, par):
-  fit_evaluated = 0*x
+  fit_evaluated = []
   for m in range(len(par)):
-    fit_evaluated += fit_function(x, *par[m])
-  fit_evaluated /= len(par)
-  return fit_evaluated
+    fit_evaluated.append(fit_function(x, *par[m]))
+  return np.array(fit_evaluated)
+
+
+def average_fit(x, par):
+  fit = evaluate_fit(x, par)
+  mean = np.mean( fit, axis=0)
+  return mean
+
 
 def plot_fit(par, center, width):
   x = np.linspace( -width, width, 101)
-  plot.plot( x, evaluate_fit(x, par) )
+  y = average_fit(x, par)
+  plot.plot( x, y )
 
 
 def read_data( nruns ):
@@ -81,9 +88,9 @@ def read_data( nruns ):
   return wl_f
 
 
-def window_smooth( x, wl_f, width ):
+def window_smooth( x, wl_f, width, eval = 0 ):
   par = fit_window(wl_f, x, width)
-  return evaluate_fit(x, par)
+  return evaluate_fit(0, par)
 
 
 def plot_window_fit( nruns, center, width ):
@@ -109,14 +116,48 @@ def plot_smoothing( nruns, width, max ):
   wl_f_fit = []
   x = np.linspace(0, sigma.shape[0]-1, sigma.shape[0])
   for point in x:
-    print(point)
-    wl_f_fit.append(window_smooth( point, wl_f, width ))
+    value = window_smooth(point, wl_f, width).mean(axis=0)
+    wl_f_fit.append(value)
 
   plot.plot( x,  np.array(wl_f_fit))
 
   plot.show()
 
 
+def average_sign( nruns, width, max ):
+  wl_f = read_data( nruns )
+  wl_f = wl_f[:,:max]
+
+  weights = []
+  for x in range(max):
+    free_energy = window_smooth( x, wl_f, width )
+    weight = np.exp(free_energy) * ( 1 - x%2*2 )
+    weights.append(weight)
+  weights = np.array(weights).transpose()
+
+  sign = np.sum(weights, axis=1)
+  mean = np.mean(sign)
+  sigma = np.std(sign)/(np.sqrt(sign.shape[0]-1))
+  return [mean, sigma]
+  
+
+
 if __name__ == "__main__":
-  plot_smoothing(20, 4, 40)
+  if len(sys.argv) > 3 :
+    nruns = int(sys.argv[1])
+    width = int(sys.argv[2])
+    max_sector = int(sys.argv[3])
+    if len(sys.argv) > 4:
+      do_plot = (sys.argv[4] == 'plot')
+    else :
+      do_plot = False
+  else:
+    print("usage: fit.py Nruns window_width max_sector ")
+
+  if do_plot:
+    plot_smoothing(nruns, width, max_sector)
+  else:
+    mean, sigma = average_sign(nruns, width, max_sector)
+    print(mean, sigma)
+
   
